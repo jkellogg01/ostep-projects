626dcd6a9f43e26f0f5ead21a1493b6776936ec9
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-10-07 10:41:48 -0600

    makefile
    also ran wcat.c and wgrep.c through clangd formatter, my natural enemy

--- initial-utilities/wcat.c
+++ initial-utilities/wcat.c
@@ -1,26 +1,26 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void print_file(const char* path) {
-	FILE* fp = fopen(path, "r");
-	if (fp == NULL) {
-		printf("wcat: cannot open file\n");
-		exit(1);
-	}
+void print_file(const char *path) {
+  FILE *fp = fopen(path, "r");
+  if (fp == NULL) {
+    printf("wcat: cannot open file\n");
+    exit(1);
+  }
 
-	char* buf = (char*)malloc(1024);
-	char* result = fgets(buf, sizeof(buf), fp);
-	while (result != NULL) {
-		printf("%s", buf);
-		result = fgets(buf, sizeof(buf), fp);
-	}
-	free(buf);
-	fclose(fp);
+  char *buf = (char *)malloc(1024);
+  char *result = fgets(buf, sizeof(buf), fp);
+  while (result != NULL) {
+    printf("%s", buf);
+    result = fgets(buf, sizeof(buf), fp);
+  }
+  free(buf);
+  fclose(fp);
 }
 
-int main(int argc, const char* argv[]) {
-	for (int i = 1; i < argc; ++i) {
-		print_file(argv[i]);
-	}
-	return 0;
+int main(int argc, const char *argv[]) {
+  for (int i = 1; i < argc; ++i) {
+    print_file(argv[i]);
+  }
+  return 0;
 }
--- initial-utilities/wgrep.c
+++ initial-utilities/wgrep.c
@@ -1,40 +1,41 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
-void grep_file(FILE* fp, const char* search_term) {
-	size_t st_len = strlen(search_term);
+void grep_file(FILE *fp, const char *search_term) {
+  size_t st_len = strlen(search_term);
 
-	size_t buf_size = 1024;
-	char* buf = (char*)malloc(buf_size);
-	ssize_t bytes_read = getline(&buf, &buf_size, fp);
-	while (bytes_read != -1) {
-		for (size_t i = 0; i + st_len < bytes_read; ++i) {
-			int cmp = memcmp(buf + i, search_term, st_len);
-			if (cmp != 0) continue;
-			printf("%s", buf);
-		}
-		bytes_read = getline(&buf, &buf_size, fp);
-	}
-	free(buf);
-	return;
+  size_t buf_size = 1024;
+  char *buf = (char *)malloc(buf_size);
+  ssize_t bytes_read = getline(&buf, &buf_size, fp);
+  while (bytes_read != -1) {
+    for (size_t i = 0; i + st_len < bytes_read; ++i) {
+      int cmp = memcmp(buf + i, search_term, st_len);
+      if (cmp != 0)
+        continue;
+      printf("%s", buf);
+    }
+    bytes_read = getline(&buf, &buf_size, fp);
+  }
+  free(buf);
+  return;
 }
 
-int main(int argc, const char* argv[]) {
-	if (argc <= 1) {
-		printf("wgrep: searchterm [file ...]\n");
-		return 1;
-	}
-	const char* search_term = argv[1];
-	if (argc == 2) {
-		grep_file(stdin, search_term);
-		return 0;
-	}
-	for (int i = 2; i < argc; ++i) {
-		FILE* fp = fopen(argv[i], "r");
-		grep_file(fp, search_term);
-		fclose(fp);
-	}
-	return 0;
+int main(int argc, const char *argv[]) {
+  if (argc <= 1) {
+    printf("wgrep: searchterm [file ...]\n");
+    return 1;
+  }
+  const char *search_term = argv[1];
+  if (argc == 2) {
+    grep_file(stdin, search_term);
+    return 0;
+  }
+  for (int i = 2; i < argc; ++i) {
+    FILE *fp = fopen(argv[i], "r");
+    grep_file(fp, search_term);
+    fclose(fp);
+  }
+  return 0;
 }
